01979d12b1f7c302e2664cc312f53292e66058ec
Eric Gallager <egall@gwmail.gwu.edu>
2017-11-23 19:58:04 -0500

    Add materials from email Nintendo sent me. #ArchivingMiiverse

---
+++ d3esbfg30x759i.cloudfront.net/js/post_index.js
@@ -0,0 +1,5 @@
+var post_index = [{"file_name":"./assets/user/js/posts/2014.js","var_name":"posts2014"},
+{"file_name":"./assets/user/js/posts/2015.js","var_name":"posts2015"},
+{"file_name":"./assets/user/js/posts/2016.js","var_name":"posts2016"},
+{"file_name":"./assets/user/js/posts/2017.js","var_name":"posts2017"},
+];
